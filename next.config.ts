750c54193967ec7f1038e208df83932c0dcb68ff
roopwara <roopeswara123456@gmail.com>
2025-10-21 09:27:19 +0530

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: process.env.GITHUB_ACTIONS && '/john-VRSR',
+  images: { unoptimized: true },
   typescript: {
     ignoreBuildErrors: true,
   },
